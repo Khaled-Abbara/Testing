8652b4f4ff329ff74d9e43f820f7c9851be8213b
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2024-09-24 18:49:19 +0800

    Update cool.ts

--- cool.ts
+++ cool.ts
@@ -1,3 +1,15 @@
 const name: string = "khaled"
 
 console.log(name);
+
+let age: number = 18
+
+console.log(age)
+
+var colourful: boolean = true
+
+function greeting(name: string, age: number, colourful: boolean) {
+console.log("Hi ${name}, you are ${age}, and you are ${colourful}")
+}
+
+greeting(name, age, colourful);
